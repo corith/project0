71f47ec6dac5cecfdcbb749ff93cd4a692d2a9c3
EC2 Default User <ec2-user@ip-172-31-40-146.us-east-2.compute.internal>
2020-12-10 21:57:57 +0000

    Staging a working version with plan of implemeneting env variables instead of raw strings

--- src/main/java/com/revature/project0/util/JDBCUtility.java
+++ src/main/java/com/revature/project0/util/JDBCUtility.java
@@ -9,13 +9,17 @@ import java.sql.SQLException;
 public class JDBCUtility {
 
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/corysebastian";
+//        String url = "jdbc:postgresql://localhost:5432/corysebastian";
         String username = "postgres";
         String password = "";
+	String url = "jdbc:postgresql://project0database.cz0mwufyd0pp.us-east-2.rds.amazonaws.com:5432/p0database";
+	String un  = "corith";
+	String pw  = "project044";
 
         Connection connection;
         DriverManager.registerDriver(new Driver());
-        connection = DriverManager.getConnection(url, username, password);
+        //connection = DriverManager.getConnection(url, username, password);
+        connection = DriverManager.getConnection(url, un, pw);
         return connection;
     }
 
